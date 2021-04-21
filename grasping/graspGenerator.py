320cfa6707a1f168f892c55377e6d2e7914a3c87
Bhargav Chandaka <bhargav2900@gmail.com>
2021-04-21 16:58:47 -0500

    Tested Knight Grasps

--- grasping/graspGenerator.py
+++ grasping/graspGenerator.py
@@ -11,16 +11,14 @@ from known_grippers import *
 sys.path.append("../grasping")
 from loadObjs import *
 from AntipodalGrasp import *
-
+import os
+if os.path.exists("chess_grasps.json"):
+  os.remove("chess_grasps.json")
 PHYSICS_SIMULATION = False  #not implemented correctly yet
-
+VIEW_GRASPS = False # Visualizes grasps when True, writes to grasp db otherwise
 if __name__ == '__main__':
     # Load the robot / world file
     fn = "./main.xml"
-
-
-
-
     gripper = robotiq_85_kinova_gen3
     db = GraspDatabase(gripper)
     try: db.load("../grasping/chess_grasps.json")
@@ -29,20 +27,21 @@ if __name__ == '__main__':
     piece_names = ["Pawn","Rook","Bishop","Knight","Queen","King"]
     for name in piece_names:
         world = WorldModel()
-        res = world.readFile(fn)
+        if not VIEW_GRASPS:
+            res = world.readFile(fn)
+            if not res:
+                print("Could not open world file")
+                exit(0)
+            # Initialize Robot
+            robot = world.robot(0)
+            qmin,qmax = robot.getJointLimits()
+            for i in range(len(qmin)):
+                if qmax[i] - qmin[i] > math.pi*2:
+                    qmin[i] = -float('inf')
+                    qmax[i] = float('inf')
+            robot.setJointLimits(qmin,qmax)
         vis.add("world",world)
-        if not res:
-            print("Could not open world file")
-            exit(0)
-        # Initialize Robot
-        robot = world.robot(0)
-        qmin,qmax = robot.getJointLimits()
-        for i in range(len(qmin)):
-            if qmax[i] - qmin[i] > math.pi*2:
-                qmin[i] = -float('inf')
-                qmax[i] = float('inf')
-        robot.setJointLimits(qmin,qmax)
-        
+
         piece = loadPiece(world, name)[0][1]
         for i in range(world.numRigidObjects()):
             obj = world.rigidObject(i)
@@ -56,19 +55,22 @@ if __name__ == '__main__':
             db.add_object(piece.getName())
 
         t0 = time.time()
-        grasps = antipodal_grasp_sample_volume(gripper,piece,20)
+        grasps = antipodal_grasp_sample_volume(gripper,piece,50)
         t1 = time.time()
         print("Sampled grasps in",t1-t0,"s, min scoring grasp",grasps[0][1], "numgrasps:", len(grasps))
         maxScore = 0
         for i,(g,s) in enumerate(grasps):
-            db.add_grasp(piece, g.genGrasp(robot))
+            if not VIEW_GRASPS:
+                db.add_grasp(piece, g.genGrasp(robot))
             maxScore = max(maxScore,g.finger_width)
-            # name = "grasp{}".format(i)
-            # u = math.exp(-(s-grasps[0][1])*2)
-            # g.add_to_vis(name) 
+            if VIEW_GRASPS:
+                name = "grasp{}".format(i)
+                u = math.exp(-(s-grasps[0][1])*2)
+                g.add_to_vis(name) 
         print("Piece:",piece.getName(), "maxScore:", maxScore)
-        vis.add(piece.getName(),piece)
-        # vis.run()
+        if VIEW_GRASPS:
+            vis.add(piece.getName(),piece)
+            vis.run()
     db.save("chess_grasps.json")
 
 
